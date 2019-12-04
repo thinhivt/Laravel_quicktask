aee290a25a404dc394b4474ec9f87f0381887f50
thinhivt <thinhdanghuu94@gmail.com>
2019-12-04 08:31:00 +0700

    update quickstart

---
+++ quickstart/resources/views/edit_task.blade.php
@@ -0,0 +1,44 @@
+<!-- resources/views/edit_tasks.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Update Task
+                </div>
+
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+
+                    <!-- Edit1 Task Form -->
+                    <form action="{{ route('update_task', $task->id)}}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            <label for="task-name" class="col-sm-3 control-label">Task</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control" value="{{ $task->name }}">
+                            </div>
+                        </div>
+
+                        <!-- Update Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    Update
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- quickstart/routes/web.php
+++ quickstart/routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+* Route return home view
+*/
+Route::get('/', 'TaskController@index')->name('index_task');
+
+/**
+* Route redirect to add task
+*/
+Route::post('add_task', 'TaskController@store')->name('add_task');
+
+/**
+* Route redirect to delete task
+*/
+Route::delete('/task/{id}', 'TaskController@destroy')->name('delete_task');
+
+/**
+* Route redirect to update task
+*/
+Route::put('/task/{id}', 'TaskController@update')->name('update_task');
+
+/**
+* Route get form edit
+*/
+Route::get('/edit/{id}', 'TaskController@edit')->name('edit_task');
+
+
